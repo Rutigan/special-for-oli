a736fa5957e4dc1d64c20843b708e52a31de694e
Rutigan <rutyganOfficial@gmail.com>
2021-10-29 17:38:12 +1000

    Изменение в структуре проекта

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express') ;
+const path = require('path') ;
+
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(path.resolve(__dirname + '/public')));
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'public',  'index.html'))
+    
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server was started on port: ${PORT}`);
+
+})
